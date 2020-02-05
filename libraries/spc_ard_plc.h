17c8bc224c45f614d901c3ed2b45923595e38bf7
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2020-02-05 14:02:09 +0100

    update

---
+++ libraries/spc_ard_plc.h
@@ -0,0 +1,30 @@
+/*
+  spc_ard_plc.h - Library for ARDUINO PLC
+  Created by h.bendali, ... 4, 2020.
+  Released into the public domain.
+*/
+#ifndef SPC_ARD_PLC_h
+#define SPC_ARD_PLC_h
+
+#include "Arduino.h"
+
+#define ARD_PLC_A10 1
+#define _ON(pin)    digitalWrite(pin, HIGH)
+#define _OFF(pin)   digitalWrite(pin, LOW)
+
+class SPC_ARD_PLC
+{
+  public:
+    void SPC_ARD_PLC(int board);
+    
+    byte extAdr[];
+    byte extReg[];
+    byte extRead(unsigned char i);
+    void extWrite(int i, int value);
+    void extRef();
+  private:
+    byte _extAdr=0;
+    byte _plc=0;
+};
+
+#endif
